7d5eac2131d7fb5f0daeaa341254cbe2206ddb00
SashaKukurika <Kukurika.sasha@gmail.com>
2023-09-25 16:28:00 +0300

    optimize request to db

--- src/orders/orders.repository.ts
+++ src/orders/orders.repository.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 
-import { OrderFieldEnum } from '../common/enums/order-field.enum';
 import { OrderQueryDto } from '../common/query/order.query.dto';
 import { Orders } from './entitys/orders.entity';
 
@@ -16,86 +15,87 @@ export class OrdersRepository extends Repository<Orders> {
     query.fieldOrder = query.fieldOrder || 'ASC';
 
     const queryBuilder = await this.createQueryBuilder('orders');
-
-    switch (query.field) {
-      case OrderFieldEnum.NAME:
-        queryBuilder.orderBy(`orders.${OrderFieldEnum.NAME}`, query.fieldOrder);
-        break;
-      case OrderFieldEnum.SURNAME:
-        queryBuilder.orderBy(
-          `orders.${OrderFieldEnum.SURNAME}`,
-          query.fieldOrder,
-        );
-        break;
-      case OrderFieldEnum.EMAIL:
-        queryBuilder.orderBy(
-          `orders.${OrderFieldEnum.EMAIL}`,
-          query.fieldOrder,
-        );
-        break;
-      case OrderFieldEnum.PHONE:
-        queryBuilder.orderBy(
-          `orders.${OrderFieldEnum.PHONE}`,
-          query.fieldOrder,
-        );
-        break;
-      case OrderFieldEnum.AGE:
-        queryBuilder.orderBy(`orders.${OrderFieldEnum.AGE}`, query.fieldOrder);
-        break;
-      case OrderFieldEnum.COURSE:
-        queryBuilder.orderBy(
-          `orders.${OrderFieldEnum.COURSE}`,
-          query.fieldOrder,
-        );
-        break;
-      case OrderFieldEnum.COURSE_FORMAT:
-        queryBuilder.orderBy(
-          `orders.${OrderFieldEnum.COURSE_FORMAT}`,
-          query.fieldOrder,
-        );
-        break;
-      case OrderFieldEnum.COURSE_TYPE:
-        queryBuilder.orderBy(
-          `orders.${OrderFieldEnum.COURSE_TYPE}`,
-          query.fieldOrder,
-        );
-        break;
-      case OrderFieldEnum.STATUS:
-        queryBuilder.orderBy(
-          `orders.${OrderFieldEnum.STATUS}`,
-          query.fieldOrder,
-        );
-        break;
-      case OrderFieldEnum.SUM:
-        queryBuilder.orderBy(`orders.${OrderFieldEnum.SUM}`, query.fieldOrder);
-        break;
-      case OrderFieldEnum.ALREADYPAID:
-        queryBuilder.orderBy(
-          `orders.${OrderFieldEnum.ALREADYPAID}`,
-          query.fieldOrder,
-        );
-        break;
-      case OrderFieldEnum.GROUP:
-        queryBuilder.orderBy(
-          `orders.${OrderFieldEnum.GROUP}`,
-          query.fieldOrder,
-        );
-        break;
-      case OrderFieldEnum.CREATED_AT:
-        queryBuilder.orderBy(
-          `orders.${OrderFieldEnum.CREATED_AT}`,
-          query.fieldOrder,
-        );
-        break;
-      case OrderFieldEnum.MANAGER:
-        queryBuilder.orderBy(
-          `orders.${OrderFieldEnum.MANAGER}`,
-          query.fieldOrder,
-        );
-        break;
-      default:
-        queryBuilder.orderBy('orders.id', 'DESC');
-    }
+    if (query.field)
+      await queryBuilder.orderBy(`orders.${query.field}`, query.fieldOrder);
+    // switch (query.field) {
+    //   case OrderFieldEnum.NAME:
+    //     queryBuilder.orderBy(`orders.${OrderFieldEnum.NAME}`, query.fieldOrder);
+    //     break;
+    //   case OrderFieldEnum.SURNAME:
+    //     queryBuilder.orderBy(
+    //       `orders.${OrderFieldEnum.SURNAME}`,
+    //       query.fieldOrder,
+    //     );
+    //     break;
+    //   case OrderFieldEnum.EMAIL:
+    //     queryBuilder.orderBy(
+    //       `orders.${OrderFieldEnum.EMAIL}`,
+    //       query.fieldOrder,
+    //     );
+    //     break;
+    //   case OrderFieldEnum.PHONE:
+    //     queryBuilder.orderBy(
+    //       `orders.${OrderFieldEnum.PHONE}`,
+    //       query.fieldOrder,
+    //     );
+    //     break;
+    //   case OrderFieldEnum.AGE:
+    //     queryBuilder.orderBy(`orders.${OrderFieldEnum.AGE}`, query.fieldOrder);
+    //     break;
+    //   case OrderFieldEnum.COURSE:
+    //     queryBuilder.orderBy(
+    //       `orders.${OrderFieldEnum.COURSE}`,
+    //       query.fieldOrder,
+    //     );
+    //     break;
+    //   case OrderFieldEnum.COURSE_FORMAT:
+    //     queryBuilder.orderBy(
+    //       `orders.${OrderFieldEnum.COURSE_FORMAT}`,
+    //       query.fieldOrder,
+    //     );
+    //     break;
+    //   case OrderFieldEnum.COURSE_TYPE:
+    //     queryBuilder.orderBy(
+    //       `orders.${OrderFieldEnum.COURSE_TYPE}`,
+    //       query.fieldOrder,
+    //     );
+    //     break;
+    //   case OrderFieldEnum.STATUS:
+    //     queryBuilder.orderBy(
+    //       `orders.${OrderFieldEnum.STATUS}`,
+    //       query.fieldOrder,
+    //     );
+    //     break;
+    //   case OrderFieldEnum.SUM:
+    //     queryBuilder.orderBy(`orders.${OrderFieldEnum.SUM}`, query.fieldOrder);
+    //     break;
+    //   case OrderFieldEnum.ALREADYPAID:
+    //     queryBuilder.orderBy(
+    //       `orders.${OrderFieldEnum.ALREADYPAID}`,
+    //       query.fieldOrder,
+    //     );
+    //     break;
+    //   case OrderFieldEnum.GROUP:
+    //     queryBuilder.orderBy(
+    //       `orders.${OrderFieldEnum.GROUP}`,
+    //       query.fieldOrder,
+    //     );
+    //     break;
+    //   case OrderFieldEnum.CREATED_AT:
+    //     queryBuilder.orderBy(
+    //       `orders.${OrderFieldEnum.CREATED_AT}`,
+    //       query.fieldOrder,
+    //     );
+    //     break;
+    //   case OrderFieldEnum.MANAGER:
+    //     queryBuilder.orderBy(
+    //       `orders.${OrderFieldEnum.MANAGER}`,
+    //       query.fieldOrder,
+    //     );
+    //     break;
+    //   default:
+    //     queryBuilder.orderBy('orders.id', 'DESC');
+    // }
 
     if (query.name)
       await queryBuilder.andWhere(`orders.name LIKE :some`, {
